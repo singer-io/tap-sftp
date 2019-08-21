7ca199d80fa8d009f241d16d60e2cb990b36af6e
Andy Lu <andy@stitchdata.com>
2019-08-21 20:13:28 +0000

    WIP: More attempts at making FTPS docker images work

--- spike/ftps/connect.py
+++ spike/ftps/connect.py
@@ -5,7 +5,14 @@
 
 
 from ftplib import FTP_TLS
+import ssl
 
+
+# Attempt to make a SSL Context object where we disable certificate verification
+# Maybe works? Could not get FTPS server working.
+ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ctx.options |= ssl.OP_NO_SSLv3
+ctx.verify_mode = ssl.CERT_NONE
 
 HOST = '172.17.0.2'
 PORT = 21
@@ -14,10 +21,11 @@ PASS = 'ftptest'
 
 
 def connect():
-    ftp = FTP_TLS(host=HOST)
+    ftp = FTP_TLS(host=HOST, context=ctx)
     ftp.login(user=USER, passwd=PASS)
     return ftp
 
 
 ftp = connect()
+ftp.set_debuglevel(2)
 ftp.prot_p()
