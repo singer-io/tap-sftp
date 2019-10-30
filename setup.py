b79541167d07a5c611e2494f6eb6cff9e03f379c
Kyle Allan <kyle@stitchdata.com>
2019-10-30 19:04:20 +0000

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="0.0.2",
+    version="0.0.3",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
