161897382299b379a331144cdeab41e07415ede1
cosimon <cosimon@users.noreply.github.com>
2019-10-31 13:17:16 -0400

    Bump to v0.0.4 (#11)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="0.0.3",
+    version="0.0.4",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
