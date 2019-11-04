ef3c89853ab409b67186096f23ed060773b71de6
Collin Simon <csimon@talend.com>
2019-11-04 14:19:41 +0000

    Bump to v0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="0.0.5",
+    version="0.0.6",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
