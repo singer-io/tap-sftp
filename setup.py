03c09cf251275b6c00fa3b4ac6fadd2a40731c74
Collin Simon <csimon@talend.com>
2019-11-04 21:37:28 +0000

    Bump singer-python to v5.9.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_sftp"],
     install_requires=[
-        "singer-python==5.8.1",
+        "singer-python==5.9.0",
         'paramiko==2.6.0',
         'backoff==1.8.0',
         'singer-encodings==0.0.5',
