1f9b97917ebfc699a9e2bf0d460c78daec9589ad
cosimon <cosimon@users.noreply.github.com>
2019-11-05 15:19:10 -0500

    use delimiter option during sync (#17)
    * use delimiter option during sync
    
    * Update singer-encodings version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "singer-python==5.9.0",
         'paramiko==2.6.0',
         'backoff==1.8.0',
-        'singer-encodings==0.0.6',
+        'singer-encodings==0.0.7',
         'terminaltables==3.1.0',
     ],
     extras_require={
