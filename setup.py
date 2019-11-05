8b6593d658cf4e33259a1ff8ff6485beeae76f17
cosimon <cosimon@users.noreply.github.com>
2019-11-05 13:00:24 -0500

    Bump to v0.0.7 (#16)
    * Bump to v0.0.7
    
    * Commit to trigger circle

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="0.0.6",
+    version="0.0.7",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
