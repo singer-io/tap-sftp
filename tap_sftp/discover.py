55859dd24bd670476334b07f9918043b9aa96162
cosimon <cosimon@users.noreply.github.com>
2019-11-05 11:46:25 -0500

    Refactor/share schema generation code (#15)
    * remove sampling / conversion to a shared library
    
    * remove unneccessary import
    
    * Bump the singer-encodings version to use the shared code

--- tap_sftp/discover.py
+++ tap_sftp/discover.py
@@ -1,7 +1,11 @@
 import json
+import singer
+
+from singer_encodings import json_schema
 from singer import metadata
 from tap_sftp import client
-from tap_sftp import sampling
+
+LOGGER= singer.get_logger()
 
 def discover_streams(config):
     streams = []
@@ -11,7 +15,8 @@ def discover_streams(config):
 
     tables = json.loads(config['tables'])
     for table_spec in tables:
-        schema = sampling.get_sampled_schema_for_table(conn, table_spec)
+        LOGGER.info('Sampling records to determine table JSON schema "%s".', table_spec['table_name'])
+        schema = json_schema.get_schema_for_table(conn, table_spec)
         stream_md = metadata.get_standard_metadata(schema,
                                                    key_properties=table_spec.get('key_properties'),
                                                    replication_method='INCREMENTAL')
