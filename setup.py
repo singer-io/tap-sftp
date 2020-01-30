f5961f9d8010c51343d272b642bb629f498323ee
nick-mccoy <nick@stitchdata.com>
2020-01-30 20:57:56 +0000

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="0.0.9",
+    version="1.0.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
