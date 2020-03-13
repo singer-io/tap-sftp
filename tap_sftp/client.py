f6fadc94dc07b0763f3b40f3df119c40d5e9a8e4
Dan Mosora <dan@stitchdata.com>
2020-03-13 21:00:01 +0000

    Default to current time so that sync extracts these files

--- tap_sftp/client.py
+++ tap_sftp/client.py
@@ -115,9 +115,9 @@ class SFTPConnection():
 
                 last_modified = file_attr.st_mtime
                 if last_modified is None:
-                    LOGGER.warning("Cannot read m_time for file %s, defaulting to epoch time 0",
+                    LOGGER.warning("Cannot read m_time for file %s, defaulting to current epoch time",
                                    os.path.join(prefix, file_attr.filename))
-                    last_modified = 0
+                    last_modified = datetime.utcnow().timestamp()
 
                 # NB: SFTP specifies path characters to be '/'
                 #     https://tools.ietf.org/html/draft-ietf-secsh-filexfer-13#section-6
