34badb28c841a6fe495d266f5720c13ca843d074
Cristian <ingcrengifo@outlook.com>
2021-02-26 13:11:05 -0500

    update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "singer-python==5.9.0",
         'paramiko==2.6.0',
         'backoff==1.8.0',
-        'singer-encodings==0.0.8',
+        'singer-encodings @ git+https://github.com/INGCRENGIFO/singer-encodings.git#egg=singer-encodings',
         'terminaltables==3.1.0',
     ],
     extras_require={
