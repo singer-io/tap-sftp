a997415e0ef96afe10171c639091345c8bed6edf
zachharris1 <69470481+zachharris1@users.noreply.github.com>
2021-07-22 11:02:45 -0400

    bump 1.1.0 (#34)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="1.0.2",
+    version="1.1.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
