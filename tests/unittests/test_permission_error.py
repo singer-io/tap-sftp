7983a5643b470580f76f648dad50a6b814380608
Harsh <80324346+harshpatel4crest@users.noreply.github.com>
2021-07-23 19:50:35 +0530

    TDL-14193: Getting 'NoneType' object is not iterable error on passing gz file containing empty csv file (#32)
    * added code for handling empty csv in gz
    
    * updated some code
    
    * updated code
    
    * updated logger info message
    
    * changed variable name
    
    * added unittest
    
    * added code change for permission denied error
    
    * added code for handling permission error in get_file_handle
    
    * updated the code as per the comments
    
    * updated a function's desc
    
    * changed raise statement
    
    * updated code according to the comments
    
    * added code for handling zip files
    
    * updated code for zip file
    
    * updated variable name
    
    * added handling of zip files
    
    * break if file is non empty
    
    * support for handling empty files

---
+++ tests/unittests/test_permission_error.py
@@ -0,0 +1,89 @@
+import unittest
+from unittest import mock
+import tap_sftp.client as client
+import tap_sftp.sync as sync
+import paramiko
+
+@mock.patch("tap_sftp.client.SFTPConnection.sftp")
+@mock.patch("tap_sftp.client.LOGGER.warn")
+class TestPermissionError(unittest.TestCase):
+
+    def test_no_error(self, mocked_logger, mocked_connect):
+
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_connect.open.side_effect = mock.mock_open()
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        file_handle = conn.get_file_handle({"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"})
+        self.assertEquals(0, mocked_logger.call_count)
+
+    def test_file_opening_error(self, mocked_logger, mocked_connect):
+
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_connect.open.side_effect = OSError
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        try:
+            file_handle = conn.get_file_handle({"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"})
+        except OSError:
+            # check if logger is called if logger is called in the function 
+            # then error has occurred otherwise not
+            mocked_logger.assert_called_with("Skipping %s file because it is unable to be read.", "/root_dir/file.csv.gz")
+
+    def test_permission_error(self, mocked_logger, mocked_connect):
+
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_connect.open.side_effect = PermissionError("Permission denied")
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        try:
+            file_handle = conn.get_file_handle({"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"})
+        except OSError:
+            # check if logger is called if logger is called in the function 
+            # then error has occurred otherwise not
+            mocked_logger.assert_called_with("Skipping %s file because you do not have enough permissions.", "/root_dir/file.csv.gz")
+
+    @mock.patch("tap_sftp.stats.add_file_data")
+    @mock.patch("singer_encodings.csv.get_row_iterators")
+    def test_no_error_during_sync(self, mocked_get_row_iterators, mocked_stats, mocked_logger, mocked_connect):
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_get_row_iterators.return_value = []
+        mocked_connect.open.side_effect = mock.mock_open()
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        rows_synced = sync.sync_file(conn, {"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"}, None, {"key_properties": ["id"], "delimiter": ","})
+        # check if "csv.get_row_iterators" is called if it is called then error has not occurred
+        # if it is not called then error has occured and function returned from the except block
+        self.assertEquals(1, mocked_get_row_iterators.call_count)
+
+    @mock.patch("singer_encodings.csv.get_row_iterators")
+    def test_permisison_error_during_sync(self, mocked_get_row_iterators, mocked_logger, mocked_connect):
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_get_row_iterators.return_value = []
+        mocked_connect.open.side_effect = PermissionError("Permission denied")
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        rows_synced = sync.sync_file(conn, {"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"}, None, {"key_properties": ["id"], "delimiter": ","})
+        # check if "csv.get_row_iterators" is called if it is called then error has not occurred
+        # if it is not called then error has occured and function returned from the except block
+        self.assertEquals(0, mocked_get_row_iterators.call_count)
+        mocked_logger.assert_called_with("Skipping %s file because you do not have enough permissions.", "/root_dir/file.csv.gz")
+
+    @mock.patch("singer_encodings.csv.get_row_iterators")
+    def test_oserror_during_sync(self, mocked_get_row_iterators, mocked_logger, mocked_connect):
+        mocked_connect.side_effect = paramiko.SFTPClient
+        mocked_get_row_iterators.return_value = []
+        mocked_connect.open.side_effect = OSError
+
+        conn = client.SFTPConnection("10.0.0.1", "username", port="22")
+
+        rows_synced = sync.sync_file(conn, {"filepath": "/root_dir/file.csv.gz", "last_modified": "2020-01-01"}, None, {"key_properties": ["id"], "delimiter": ","})
+        # check if "csv.get_row_iterators" is called if it is called then error has not occurred
+        # if it is not called then error has occured and function returned from the except block
+        self.assertEquals(0, mocked_get_row_iterators.call_count)
+        mocked_logger.assert_called_with("Skipping %s file because it is unable to be read.", "/root_dir/file.csv.gz")
--- tests/unittests/test_sorted_files.py
+++ tests/unittests/test_sorted_files.py
@@ -8,10 +8,9 @@ import pytz
 import singer
 
 @mock.patch("tap_sftp.client.SFTPConnection.get_files_by_prefix")
-@mock.patch("tap_sftp.client.SFTPConnection.get_files_matching_pattern")
 class TestSortedFiles(unittest.TestCase):
 
-    def test_sorted_files(self, mocked_matching_files, mocked_all_files):
+    def test_sorted_files(self, mocked_all_files):
         conn = client.SFTPConnection("10.0.0.1", "username", port="22")
 
         files_list = [
@@ -42,9 +41,7 @@ class TestSortedFiles(unittest.TestCase):
 
         mocked_all_files.return_value = files_list
 
-        mocked_matching_files.return_value = files_list[:4]
-
-        files = conn.get_files("/root", "file.*.csv")
+        files = conn.get_files("/root", "file[0-9].csv")
 
         # expected files in increasing order of "last_modified"
         expected_files_list = ["/root/file1.csv", "/root/file3.csv", "/root/file2.csv", "/root/file4.csv"]
@@ -52,7 +49,7 @@ class TestSortedFiles(unittest.TestCase):
 
         self.assertEquals(expected_files_list, actual_files_list)
 
-    def test_sorted_files_negative(self, mocked_matching_files, mocked_all_files):
+    def test_sorted_files_negative(self, mocked_all_files):
         conn = client.SFTPConnection("10.0.0.1", "username", port="22")
 
         files_list = [
@@ -75,11 +72,9 @@ class TestSortedFiles(unittest.TestCase):
 
         mocked_all_files.return_value = files_list
 
-        mocked_matching_files.return_value = files_list[:2]
-
         # setting "modified_since" to now
         modified_since = singer.utils.strptime_to_utc(datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat())
-        files = conn.get_files("/root", "file.*.csv", modified_since)
+        files = conn.get_files("/root", "file[0-9].csv", modified_since)
 
         # as all the modified date is lesser than "modified_since" thus, no files will be returned
         expected_files_list = []
