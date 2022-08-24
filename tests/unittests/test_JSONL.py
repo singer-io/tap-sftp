e5b22d1526e9a047df0c6ae7077493dbba252d78
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-08-24 15:05:43 +0530

    added test cases for row iterator function

--- tests/unittests/test_JSONL.py
+++ tests/unittests/test_JSONL.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 from parameterized import parameterized
 from tap_sftp import discover
 
@@ -10,8 +11,9 @@ class JSONLIterator:
         return self.data
 
 class Iterable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         self.data = data
+        self.name = name
 
     def read(self):
         return self.data
@@ -49,3 +51,28 @@ class TestCheckJSONLKeyProperties(unittest.TestCase):
                 iterator=records
             )
         self.assertEqual(str(e.exception), expected_data)
+
+class TestRowIterators(unittest.TestCase):
+    """Test cases to verify we call 'row_iterator' for JSONL or CSV as per the file extension"""
+    @parameterized.expand([
+        ["csv", ["test.csv", [Iterable("")]], [1, 0]],
+        ["jsonl", ["test.jsonl", [Iterable('{"id": 1}')]], [0, 1]],
+        ["zip_csv", ["test.zip", [Iterable("", "test.csv")]], [1, 0]],
+        ["zip_jsonl", ["test.zip", [Iterable("", "test.jsonl")]], [0, 1]],
+        ["gz_csv", ["test.gz", [[Iterable(""), "test.csv"]]], [1, 0]],
+        ["gz_jsonl", ["test.gz", [[Iterable(""), "test.jsonl"]]], [0, 1]],
+    ])
+    @mock.patch("tap_sftp.discover.get_JSONL_iterators")
+    @mock.patch("singer_encodings.csv.get_row_iterator")
+    @mock.patch("singer_encodings.compression.infer")
+    def test_get_row_iterators_local(self, name, test_data, expected_data, mocked_infer, mocked_get_csv_row_iterator, mocked_get_JSONL_iterators):
+        # Mock file iterable
+        mocked_infer.return_value = test_data[1]
+        options = {
+            "file_name": test_data[0]
+        }
+        # Function call
+        list(discover.get_row_iterators_local(iterable=[], options=options, infer_compression=True))
+        # Verify the call count for JSONL or CSV row_iterator
+        self.assertEqual(mocked_get_csv_row_iterator.call_count, expected_data[0])
+        self.assertEqual(mocked_get_JSONL_iterators.call_count, expected_data[1])
