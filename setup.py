a83e7f9b12dea2a65510a2e05d5fa46a12ced839
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2024-12-19 10:44:03 -0500

    removes deprecated terminaltables dependency (#47)
    * remove terminaltables dependency
    
    * update circleci nose tests
    
    * use nose2
    
    * keep scope of card to terminaltables

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-sftp",
-    version="1.2.0",
+    version="1.2.1",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
@@ -14,7 +14,6 @@ setup(
         'paramiko==2.6.0',
         'backoff==1.8.0',
         'singer-encodings==0.1.3',
-        'terminaltables==3.1.0',
     ],
     extras_require={
         'dev': [
