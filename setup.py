eaea5d1bc5146326706fa8454c1fe1b8a7438f08
Ben Allred <ben.allred@qlik.com>
2025-07-01 13:45:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_sftp"],
     install_requires=[
-        "singer-python==5.12.1",
+        "singer-python==5.13.2",
         'paramiko==2.6.0',
-        'backoff==1.8.0',
+        'backoff==1.10.0',
         'singer-encodings==0.1.3',
     ],
     extras_require={
